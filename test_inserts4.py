40dd1001f4115664b5e70a08da1051d5f1342421
ebtrader <jsiddique@gmail.com>
2023-06-12 14:52:10 -0400

    Commit

---
+++ test_inserts4.py
@@ -0,0 +1,33 @@
+from sshtunnel import SSHTunnelForwarder
+import pymysql
+
+# https://gist.github.com/riddhi89/9d53140dec7c17e63e22a0b5ab43f99f
+# https://www.linkedin.com/pulse/programmatically-access-private-rds-database-aws-from-tom-reid/
+
+with SSHTunnelForwarder(
+
+        ('ec2-44-202-61-2.compute-1.amazonaws.com'),
+        ssh_username='ubuntu',
+        ssh_pkey=r'C:\Users\jsidd\Documents\aws\test_key.pem',
+        remote_bind_address=('database-1.c3dig9vjwrmk.us-east-1.rds.amazonaws.com', 3306)
+
+) as tunnel:
+    print("****SSH Tunnel Established****")
+
+    db = pymysql.connect(
+        host='127.0.0.1', user='admin',
+        password='suite203', port=tunnel.local_bind_port
+    )
+
+    # Run sample query in the database to validate connection
+    try:
+        # Print all the databases
+        with db.cursor() as cur:
+            sql = 'insert into javeddb.tqqq_raw_temp (ticker, strike, expiration) VALUES (%s, %s, %s)'
+            val = ("INTC", 24, '2023-01-10')
+            cur.execute(sql, val)
+            db.commit()
+    finally:
+        db.close()
+
+
